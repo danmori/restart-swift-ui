2bd890c64b9d219c492c0edae5d6a53b517f1dd6
Dan Mori <danielstelzer@gmail.com>
2022-07-10 14:12:17 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Dan Mori on 10/07/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
