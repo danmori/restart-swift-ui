a917889afee85dab7876c5a36dd60faa3528ef4f
Dan Mori <danielstelzer@gmail.com>
2022-07-10 19:01:51 -0300

    Added animation

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -9,16 +9,58 @@ import SwiftUI
 
 struct HomeView: View {
     @AppStorage("onboarding") var isOnboargindViewActive = false
+    @State private var isAnimating: Bool = false
+    
     var body: some View {
         VStack(spacing: 20) {
-            Text("Home")
-                .font(.largeTitle)
-            Button(action: {
-                isOnboargindViewActive = true
-            }) {
-                Text("Restart")
+            
+            Spacer()
+            
+            ZStack {
+                CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.1)
+                
+                Image("character-2")
+                    .resizable()
+                    .scaledToFit()
+                .padding()
+                .offset(y: isAnimating ? 35 : -35)
+                .animation(
+                    Animation
+                        .easeOut(duration: 4)
+                        .repeatForever()
+                    , value: isAnimating
+                )
             }
+            
+            Text("The time that leads to mastery is dependent on the intensity of our focus.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            Spacer()
+            
+            Button(action: {
+                withAnimation {
+                    isOnboargindViewActive = true
+                }
+            }) {
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
+                Text("Restart")
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
+            }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
         }
+        .onAppear(perform: {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+                isAnimating = true
+            })
+        })
     }
 }
 
